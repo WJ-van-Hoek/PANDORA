3803abefc61fe4a140abef740ede30a57ff1ddb3
Willem_Windows <wimjoost@gmail.com>
2023-05-13 12:44:03 +0200

    first commit

---
+++ src/main/java/com/science/earth/biogeochemistry/freshwaters/PANDORA/PandoraApplication.java
@@ -0,0 +1,13 @@
+package com.science.earth.biogeochemistry.freshwaters.PANDORA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PandoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/science/earth/biogeochemistry/freshwaters/PANDORA/PandoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.science.earth.biogeochemistry.freshwaters.PANDORA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
