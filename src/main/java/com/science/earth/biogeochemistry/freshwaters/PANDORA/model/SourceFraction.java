a8de974934d54fabba79c974e91980e6634b7568
Willem_Windows <wimjoost@gmail.com>
2023-05-13 15:28:56 +0200

    fix entity builder error

---
+++ src/main/java/com/science/earth/biogeochemistry/freshwaters/PANDORA/model/SourceFraction.java
@@ -0,0 +1,31 @@
+package com.science.earth.biogeochemistry.freshwaters.PANDORA.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class SourceFraction extends AbstractBaseEntity {
+
+    private static final long serialVersionUID = 24109037202595284L;
+    
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "specie_id")
+    private Specie specie;
+    
+    private float fractional;
+    
+    @ManyToOne
+    @JoinColumn(name = "source_id")
+    private Source source;
+}
