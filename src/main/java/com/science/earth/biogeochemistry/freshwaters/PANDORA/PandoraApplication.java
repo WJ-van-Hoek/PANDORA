d742b3742b80e53bf00c4794726ce9004abeae69
Willem_Windows <wimjoost@gmail.com>
2023-05-18 12:30:09 +0200

    add messages.properties and related config. closes #16

--- src/main/java/com/science/earth/biogeochemistry/freshwaters/PANDORA/PandoraApplication.java
+++ src/main/java/com/science/earth/biogeochemistry/freshwaters/PANDORA/PandoraApplication.java
@@ -2,8 +2,10 @@ package com.science.earth.biogeochemistry.freshwaters.PANDORA;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(AppConfig.class)
 public class PandoraApplication {
 
     public static void main(String[] args) {
