41381b0f94e22fc43c099cac28ac0f4c75f4d0b6
Willem_Windows <wimjoost@gmail.com>
2023-07-12 10:48:59 +0200

    PANDORA-74 fix compile error

---
+++ pandora-data/src/main/java/com/science/earth/biogeochemistry/freshwaters/pandora/commands/AbstractBaseCommand.java
@@ -0,0 +1,22 @@
+package com.science.earth.biogeochemistry.freshwaters.pandora.commands;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
+public abstract class AbstractBaseCommand {
+
+        private Long id;
+        private String name;
+
+        protected AbstractBaseCommand(Long id, String name) {
+    	this.id = id;
+    	this.name = name;
+        }
+
+}
