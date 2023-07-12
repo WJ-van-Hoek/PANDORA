6f72ddb6a6cd7e689a47eaeaf6bf0be7f9cde0b4
Willem_Windows <wimjoost@gmail.com>
2023-07-12 11:05:57 +0200

    PANDORA-76 add AbstractBaseObject class

---
+++ pandora-data/src/main/java/com/science/earth/biogeochemistry/freshwaters/pandora/general/AbstractBaseObject.java
@@ -0,0 +1,20 @@
+package com.science.earth.biogeochemistry.freshwaters.pandora.general;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
+public abstract class AbstractBaseObject {
+    private Long id;
+    private String name;
+
+    protected AbstractBaseObject(Long id, String name) {
+	this.id = id;
+	this.name = name;
+    }
+}
