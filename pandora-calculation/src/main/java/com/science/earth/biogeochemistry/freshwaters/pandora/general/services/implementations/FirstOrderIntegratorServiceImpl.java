29f1e1a4054121ef5afbbc6c1a80d376b4c4271a
Willem_Windows <wimjoost@gmail.com>
2023-07-14 10:30:57 +0200

    PANDORA-79 add first version of FirstOrderIntegratorServiceImpl

--- pandora-calculation/src/main/java/com/science/earth/biogeochemistry/freshwaters/pandora/general/services/implementations/FirstOrderIntegratorServiceImpl.java
+++ pandora-calculation/src/main/java/com/science/earth/biogeochemistry/freshwaters/pandora/general/services/implementations/FirstOrderIntegratorServiceImpl.java
@@ -1,18 +1,35 @@
 package com.science.earth.biogeochemistry.freshwaters.pandora.general.services.implementations;
 
 import org.apache.commons.math3.ode.FirstOrderIntegrator;
+import org.apache.commons.math3.ode.nonstiff.DormandPrince853Integrator;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.science.earth.biogeochemistry.freshwaters.pandora.general.services.interfaces.FirstOrderIntegratorService;
 
 @Service
 public class FirstOrderIntegratorServiceImpl implements FirstOrderIntegratorService {
+    @Value("${integrator.method}")
+    private String integratorMethod;
+    
+    @Value("${integrator.nonstiff.params.minStep}")
+    private double minStep;
+    
+    @Value("${integrator.nonstiff.params.maxStep}")
+    private double maxStep;
+    
+    @Value("${integrator.nonstiff.params.absoluteTolerance}")
+    private double absoluteTolerance;
+    
+    @Value("${integrator.nonstiff.params.relativeTolerance}")
+    private double relativeTolerance;
 
     @Override
     public FirstOrderIntegrator getFirstOrderIntegrator() {
-
-	// TODO Auto-generated method stub
-	return null;
+	if ("dormand-prince".equalsIgnoreCase(integratorMethod)) {
+	    return new DormandPrince853Integrator(minStep, maxStep, absoluteTolerance, relativeTolerance);
+	}
+	throw new IllegalArgumentException("Invalid integration method: " + integratorMethod);
     }
 
 }
