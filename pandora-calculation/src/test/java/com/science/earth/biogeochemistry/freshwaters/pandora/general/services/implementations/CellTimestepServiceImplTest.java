08f1eb910755e0e2382075d5911e73e027b5b8a2
Willem_Windows <wimjoost@gmail.com>
2023-07-21 10:18:59 +0200

    PANDORA-86 add CellTimestepBaseObject builder

--- pandora-calculation/src/test/java/com/science/earth/biogeochemistry/freshwaters/pandora/general/services/implementations/CellTimestepServiceImplTest.java
+++ pandora-calculation/src/test/java/com/science/earth/biogeochemistry/freshwaters/pandora/general/services/implementations/CellTimestepServiceImplTest.java
@@ -1,11 +1,20 @@
 package com.science.earth.biogeochemistry.freshwaters.pandora.general.services.implementations;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.science.earth.biogeochemistry.freshwaters.pandora.general.objects.CellTimestepBaseObject;
+
 class CellTimestepServiceImplTest {
+    
+    private static final LocalDateTime T_0 = LocalDateTime.of(2023, 7, 21, 0, 0);
+    
+    private CellTimestepBaseObject cellTimestep;
+
 
     @BeforeEach
     void setUp() throws Exception {
--- pandora-data/src/main/java/com/science/earth/biogeochemistry/freshwaters/pandora/general/objects/CellTimestepBaseObject.java
+++ pandora-data/src/main/java/com/science/earth/biogeochemistry/freshwaters/pandora/general/objects/CellTimestepBaseObject.java
@@ -3,6 +3,7 @@ package com.science.earth.biogeochemistry.freshwaters.pandora.general.objects;
 import java.util.List;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
@@ -15,6 +16,20 @@ public class CellTimestepBaseObject extends AbstractBaseObject {
     List<Float> upstreamSources;
     List<Float> transformations;
     Float netPrecipitation;
+    
+    @Builder
+    public CellTimestepBaseObject(CellBaseObject cell,
+	    List<Double> y0, 
+	    List<Float> terrestrialSources, List<Float> upstreamSources, 
+	    List<Float> transformations, Float netPrecipitation) {
+	this.cell = cell;
+	this.y0 = y0;
+	this.terrestrialSources = terrestrialSources;
+	this.upstreamSources = upstreamSources;
+	this.transformations = transformations;
+	this.netPrecipitation = netPrecipitation;
+    }
+    
     
     public void setYEnd(List<Double> yEnd) {
 	this.yEnd = yEnd;
