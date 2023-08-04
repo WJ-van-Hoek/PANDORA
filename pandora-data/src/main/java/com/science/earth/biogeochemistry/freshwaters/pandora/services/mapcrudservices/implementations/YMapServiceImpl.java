9b28372f0766b8f932c66840396a83a4288ac659
Willem_Windows <wimjoost@gmail.com>
2023-08-04 15:27:35 +0200

    PANDORA-90 minor refactor

--- pandora-data/src/main/java/com/science/earth/biogeochemistry/freshwaters/pandora/services/mapcrudservices/implementations/YMapServiceImpl.java
+++ pandora-data/src/main/java/com/science/earth/biogeochemistry/freshwaters/pandora/services/mapcrudservices/implementations/YMapServiceImpl.java
@@ -1,19 +1,13 @@
 package com.science.earth.biogeochemistry.freshwaters.pandora.services.mapcrudservices.implementations;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
 
 import org.springframework.stereotype.Service;
 
 import com.science.earth.biogeochemistry.freshwaters.pandora.general.objects.CellBaseObject;
 
 @Service
-public class YMapServiceImpl implements YMapService {
-    
-    protected Map<Integer, double[]> map = new HashMap<>();
-    
+public class YMapServiceImpl extends AbstractHashMapService implements YMapService {    
     @Override
     public double[] findAtCellAndTimestep(CellBaseObject cell, LocalDateTime t) {
 	return map.get(hashCellAndTime(cell, t));
@@ -22,9 +16,5 @@ public class YMapServiceImpl implements YMapService {
     @Override
     public void saveAtCellAndTimestep(CellBaseObject cell, LocalDateTime tEnd, double[] yEnd) {
 	map.put(hashCellAndTime(cell, tEnd), yEnd);
-    }
-    
-    private int hashCellAndTime(CellBaseObject cell, LocalDateTime t) {
-	return Objects.hash(cell, t);
     }
 }
