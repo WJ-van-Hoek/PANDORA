49c280a0ad8fb914c697c8f22683098a5442ff49
Willem_Windows <wimjoost@gmail.com>
2023-08-06 15:38:50 +0200

    PANDORA-95 make a PANDORA calculation a separate kernel with main application

---
+++ pandora-calculation/src/main/java/com/science/earth/biogeochemistry/freshwaters/pandora/PandoraCalculation.java
@@ -0,0 +1,12 @@
+package com.science.earth.biogeochemistry.freshwaters.pandora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandoraCalculation {
+
+    public static void main(String[] args) {
+	SpringApplication.run(PandoraCalculation.class, args);
+    }
+}
