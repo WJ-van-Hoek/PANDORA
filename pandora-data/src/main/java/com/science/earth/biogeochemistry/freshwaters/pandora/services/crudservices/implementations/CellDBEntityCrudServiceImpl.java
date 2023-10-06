25badbbb78caa6d200d3a7f66a1a31baee41fbf0
Willem_Windows <wimjoost@gmail.com>
2023-10-06 12:47:10 +0200

    PANDORA-108 add dockerfile

---
+++ pandora-data/src/main/java/com/science/earth/biogeochemistry/freshwaters/pandora/services/crudservices/implementations/CellDBEntityCrudServiceImpl.java
@@ -0,0 +1,20 @@
+package com.science.earth.biogeochemistry.freshwaters.pandora.services.crudservices.implementations;
+
+import org.springframework.stereotype.Service;
+
+import com.science.earth.biogeochemistry.freshwaters.pandora.errors.ErrorMessageGenerator;
+import com.science.earth.biogeochemistry.freshwaters.pandora.model.CellDBEntity;
+import com.science.earth.biogeochemistry.freshwaters.pandora.repositories.CellDBEntityRepository;
+import com.science.earth.biogeochemistry.freshwaters.pandora.services.crudservices.interfaces.CellDBEntityCrudService;
+
+@Service
+public class CellDBEntityCrudServiceImpl extends AbstractBaseEntityCrudServiceImpl<CellDBEntity, CellDBEntityRepository>
+	implements CellDBEntityCrudService {
+
+    protected CellDBEntityCrudServiceImpl(CellDBEntityRepository repository,
+	    ErrorMessageGenerator errorMessageGenerator) {
+
+	super(repository, errorMessageGenerator);
+    }
+
+}
