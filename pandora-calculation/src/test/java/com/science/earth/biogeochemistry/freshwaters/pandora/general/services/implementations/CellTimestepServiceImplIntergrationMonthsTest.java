f2eeb7837179dcac1013b07af64f2684dc337100
TAMOJIT DAS <tamojitdas1997@gmail.com>
2023-12-02 01:27:48 +0530

    Squashed Commits where Few Tests files and test Cases have been added for covering LocalDateTimeServiceImpl.class

--- pandora-calculation/src/test/java/com/science/earth/biogeochemistry/freshwaters/pandora/general/services/implementations/CellTimestepServiceImplIntergrationMonthsTest.java
+++ pandora-calculation/src/test/java/com/science/earth/biogeochemistry/freshwaters/pandora/general/services/implementations/CellTimestepServiceImplIntergrationMonthsTest.java
@@ -1,5 +1,4 @@
 package com.science.earth.biogeochemistry.freshwaters.pandora.general.services.implementations;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -11,6 +10,7 @@ import java.time.temporal.ChronoUnit;
 
 import com.science.earth.biogeochemistry.freshwaters.pandora.general.services.calculation.implementations.LocalDateTimeServiceImpl;
 import com.science.earth.biogeochemistry.freshwaters.pandora.general.services.calculation.interfaces.LocalDateTimeService;
+import net.bytebuddy.implementation.bind.annotation.Super;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -38,36 +38,36 @@ import static org.mockito.ArgumentMatchers.anyDouble;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
-@TestPropertySource(locations = "classpath:test.properties")
-class CellTimestepServiceImplIntegrationTest {
+@TestPropertySource (locations = "classpath:test1.properties")
+public class CellTimestepServiceImplIntergrationMonthsTest {
     private static final Cell CELL = Cell.builder().id(1l).centerLatitude(52.08f).centerLongitude(5.67f).surfaceArea(1754.27f).nextCellId(1l).build();
     private static final LocalDate LOCAL_DATE = LocalDate.of(2023, 8, 9);
     private static final LocalTime LOCAL_TIME = LocalTime.of(0, 0);
     private static final LocalDateTime T_0 = LocalDateTime.of(LOCAL_DATE, LOCAL_TIME);
     private static List<LocalDateTime> LOCAL_DATE_TIME_LIST = new ArrayList<LocalDateTime>();
     private static final PandoraTimestep PANDORA_TIMESTEP = PandoraTimestep.builder().discharge(10d).volume(1d).build();
-    private static final double[] Y_0 = {1d,1d}; 
+    private static final double[] Y_0 = {1d,1d};
     private static final double[] TERRESTRIAL_SOURCES = {2d, 2d};
     private static final double[] UPSTREAM_SOURCES = {3d, 3d};
     private static final Double DISCHARGE = 1d;
     private static final double VOLUME = 1d;
     private static final double[] Y_END = {19.7403d, 21.5063d};
-    
+
     @Autowired
     YMapService yMapService;
-    
+
     @Autowired
     TerrestrialSourcesMapService terrestrialSourcesMapService;
-    
+
     @Autowired
     UpstreamSourcesMapService upstreamSourcesMapService;
-    
+
     @Autowired
     DischargesMapService dischargesMapService;
-    
+
     @Autowired
     VolumesMapService volumesMapService;
-    
+
     @Autowired
     CellTimestepService cellTimestepService;
 
@@ -81,34 +81,32 @@ class CellTimestepServiceImplIntegrationTest {
     LocalDateTimeServiceImpl localDateTimeServiceImpl;
 
     @BeforeEach
-    void setUp() throws Exception {
-    MockitoAnnotations.openMocks(this);
-    yMapService.cleanMap();
-	yMapService.saveAtCellAndTimestep(CELL, T_0, Y_0);
-	terrestrialSourcesMapService.cleanMap();
-	terrestrialSourcesMapService.saveAtCellAndTimestep(CELL, T_0, TERRESTRIAL_SOURCES);
-	upstreamSourcesMapService.cleanMap();
-	upstreamSourcesMapService.saveAtCellAndTimestep(CELL, PANDORA_TIMESTEP, T_0, UPSTREAM_SOURCES);
-	dischargesMapService.cleanMap();
-	dischargesMapService.saveAtCellAndTimestep(CELL, T_0, DISCHARGE);
-	volumesMapService.cleanMap();
-	volumesMapService.saveAtCellAndTimestep(CELL, T_0, VOLUME);
-	
+    void setUp() throws Exception
+    {
+        MockitoAnnotations.openMocks(this);
+        yMapService.cleanMap();
+        yMapService.saveAtCellAndTimestep(CELL, T_0, Y_0);
+        terrestrialSourcesMapService.cleanMap();
+        terrestrialSourcesMapService.saveAtCellAndTimestep(CELL, T_0, TERRESTRIAL_SOURCES);
+        upstreamSourcesMapService.cleanMap();
+        upstreamSourcesMapService.saveAtCellAndTimestep(CELL, PANDORA_TIMESTEP, T_0, UPSTREAM_SOURCES);
+        dischargesMapService.cleanMap();
+        dischargesMapService.saveAtCellAndTimestep(CELL, T_0, DISCHARGE);
+        volumesMapService.cleanMap();
+        volumesMapService.saveAtCellAndTimestep(CELL, T_0, VOLUME);
+
     }
 
+    //LocalDateTimeTest Coverage::
+
     @Test
-    void testCalculateNextTimestep() {
-    cellTimestepService.calculateNextTimestep(CELL, T_0);
-	Assertions.assertArrayEquals(Y_END,  yMapService.findAtCellAndTimestep(CELL, T_0.plusYears(1)), 1e-3);
+    void testCalculateTimeSeriesMonths() {
+
+        this.cellTimestepService.calculateTimeSeries(CELL, T_0, 1);
+        Assertions.assertNotNull ( yMapService.findAtCellAndTimestep(CELL, T_0.plusMonths(1)));
 //        Assertions.assertArrayEquals(null,  yMapService.findAtCellAndTimestep(CELL, T_0.plusYears(1)), 1e-3);
     }
 
-    @Test
-    void testCalculateTimeSeries() {
 
-        cellTimestepService.calculateTimeSeries(CELL, T_0, 1);
-        Assertions.assertArrayEquals(Y_END,  yMapService.findAtCellAndTimestep(CELL, T_0.plusYears(1)), 1e-3);
-
-    }
 
 }
