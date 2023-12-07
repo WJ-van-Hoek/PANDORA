9ef6012f3ede8828cf8d9027e4befb55e425f1d3
Willem_Windows <wimjoost@gmail.com>
2023-12-07 21:45:47 +0100

    PANDORA-125: clean up SpeciesResponseServiceImpl

--- pandora-calculation/src/main/java/com/science/earth/biogeochemistry/freshwaters/pandora/controllers/responsebodies/services/SpeciesResponseServiceImpl.java
+++ pandora-calculation/src/main/java/com/science/earth/biogeochemistry/freshwaters/pandora/controllers/responsebodies/services/SpeciesResponseServiceImpl.java
@@ -1,72 +1,28 @@
 package com.science.earth.biogeochemistry.freshwaters.pandora.controllers.responsebodies.services;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
 import org.springframework.stereotype.Service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.general.utils.json.mappers.MapToJsonMapper;
+import com.general.utils.object.mappers.ObjectToMapMapper;
 import com.science.earth.biogeochemistry.freshwaters.pandora.config.species.abstractions.Specie;
 import com.science.earth.biogeochemistry.freshwaters.pandora.controllers.responsebodies.SpeciesResponse;
 
 @Service
 public class SpeciesResponseServiceImpl implements SpeciesResponseService {
 
-    @Override
-    public SpeciesResponse postprocess(Map<String, Specie> speciesMap) {
-	List<String> speciesList = new ArrayList<>();
-	for (Map.Entry<String, Specie> entry : speciesMap.entrySet()) {
-	    speciesList.add(ObjectInspector.inspect(entry.getValue()));
-	}
-	return new SpeciesResponse(speciesList);
-    }
-}
-
-class ObjectInspector {
-    public static String inspect(Specie obj) {
-	Class<?> clazz = obj.getClass();
-	Class<?> originalClazz = clazz.getSuperclass();
-	Field[] fields = originalClazz.getDeclaredFields();
-
-	Map<String, Object> inspectionResult = new TreeMap<>((key1, key2) -> {
-	    if ("name".equals(key1)) {
-		return -1;
-	    } else if ("name".equals(key2)) {
-		return 1;
-	    } else {
-		return key1.compareTo(key2);
-	    }
-	});
-
-	for (Field field : fields) {
-	    field.setAccessible(true);
-	    String fieldName = field.getName();
-	    if (!"log".equals(fieldName)) {
-		Object value = null;
-
-		try {
-		    value = field.get(obj);
-		} catch (IllegalAccessException e) {
-		    e.printStackTrace();
+	@Override
+	public SpeciesResponse postprocess(Map<String, Specie> speciesMap) {
+		List<String> speciesList = new ArrayList<>();
+		
+		for (Map.Entry<String, Specie> entry : speciesMap.entrySet()) {
+			Map<String, Object> specieMap = ObjectToMapMapper.convertObjectToMap(entry.getValue(), true);
+			speciesList.add(MapToJsonMapper.convertMapToJson(specieMap));
 		}
-
-		inspectionResult.put(fieldName, value);
-	    }
+		return new SpeciesResponse(speciesList);
 	}
-
-	return convertMapToJson(inspectionResult);
-    }
-
-    private static String convertMapToJson(Map<String, Object> map) {
-	try {
-	    ObjectMapper objectMapper = new ObjectMapper();
-	    return objectMapper.writeValueAsString(map);
-	} catch (Exception e) {
-	    e.printStackTrace();
-	    return null;
-	}
-    }
 }
+
