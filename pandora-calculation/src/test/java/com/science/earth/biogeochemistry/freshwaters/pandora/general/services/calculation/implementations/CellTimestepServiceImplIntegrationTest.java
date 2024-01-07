10dc26a7143bd00f5d2952c7e13520505e22867e
Willem_Windows <wimjoost@gmail.com>
2024-01-07 21:39:33 +0100

    PANDORA-141: minor refactor

--- pandora-calculation/src/test/java/com/science/earth/biogeochemistry/freshwaters/pandora/general/services/calculation/implementations/CellTimestepServiceImplIntegrationTest.java
+++ pandora-calculation/src/test/java/com/science/earth/biogeochemistry/freshwaters/pandora/general/services/calculation/implementations/CellTimestepServiceImplIntegrationTest.java
@@ -1,4 +1,4 @@
-package com.science.earth.biogeochemistry.freshwaters.pandora.general.services.implementations;
+package com.science.earth.biogeochemistry.freshwaters.pandora.general.services.calculation.implementations;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
