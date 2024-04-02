8dfb87b99bc8f02be916eae464a3b91b50da2169
Willem_Ubuntu <wimjoost@gmail.com>
2024-04-02 07:55:15 +0200

    PANDORA-154: update dependencies

--- pandora-data/src/main/java/com/pandora/repositories/CellDBEntityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pandora.repositories;
-
-import com.pandora.model.CellDBEntity;
-
-public interface CellDBEntityRepository extends AbstractBaseDBEntityRepository<CellDBEntity> {
-
-}
--- pandora-web/src/main/resources/static/js/apiservices/apiConfig.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  baseUrl: 'http://localhost:9000/api',
-  // other configuration options if needed
-};
-
-export default config;
